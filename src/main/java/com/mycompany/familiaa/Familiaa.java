a99cd9e7d6fe20230b9380707c72e5d7e3206d01
[jmbbenavides] <[1942395@clases.edu.sv]>
2022-08-31 00:59:59 +0000

    Primer commit

---
+++ src/main/java/com/mycompany/familiaa/Familiaa.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.familiaa;
+
+/**
+ *
+ * @author jmbb
+ */
+public class Familiaa {
+
+    public static void main(String[] args) {
+        int family = 4;
+        System.out.println("ESTA ES MI FAMILIA");
+        System.out.println("Mi padre es: Ely");
+        System.out.println("Mi mama es: Rosa");
+        System.out.println("Mi hermana es: Eunice");
+        System.out.println("Somos ");
+        System.out.println(family);
+        System.out.println("en total");
+    }
+}
